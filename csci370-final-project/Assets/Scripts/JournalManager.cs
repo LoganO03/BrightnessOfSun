246e151d47482dca37a789b698b004d82b109ee4
ianwalker04 <150100136+ramaseta@users.noreply.github.com>
2025-04-13 11:01:03 -0500

    Created journal mechanic.

---
+++ csci370-final-project/Assets/Scripts/JournalManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class JournalManager : MonoBehaviour
+{
+    public GameObject Journal;
+    public GameObject JournalIcon;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J)) {
+            GetComponent<PlayerMovement>().enabled = !GetComponent<PlayerMovement>().enabled;
+            if (GetComponent<PlayerMovement>().enabled) {
+                Cursor.lockState = CursorLockMode.Locked;
+            } else {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            JournalIcon.gameObject.SetActive(!JournalIcon.gameObject.activeSelf);
+            Journal.gameObject.SetActive(!Journal.gameObject.activeSelf);
+        }
+    }
+}
